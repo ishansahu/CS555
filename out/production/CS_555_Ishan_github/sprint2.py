7e2ec1164958589bd20b73151a843077b8dd6053
Gireesh <gireeshsinghthakurathi@gmail.com>
2021-03-25 23:02:24 -0400

    Sprint 2 UserStory

---
+++ out/production/CS_555_Ishan_github/sprint2.py
@@ -0,0 +1,22 @@
+import gedcom_parser
+
+file_path = 'InputFiles/Project01.ged'
+
+def US05():  # US05: Marriage Before Death
+    file_ = 'gedcomTests/main_test.ged'
+    peopleList, famList = gedcom_parser.parse(file_)
+    individualFailed_list=[]
+    for fam in famList:
+        if fam.husbandId != 'NA':
+            h = gedcom_parser.findPerson(fam.husbandId, peopleList)
+            husb = h.createDeepCopy(h)
+            if fam.married != 'NA' and husb.alive == False and husb.death < fam.married:
+                husb.marriage = fam.married
+                individualFailed_list.append(husb)
+        if fam.wifeId != 'NA':
+            w = gedcom_parser.findPerson(fam.wifeId, peopleList)
+            wife = w.createDeepCopy(w)
+            if fam.married != 'NA'  and wife.alive==False and wife.death < fam.married:
+                wife.marriage = fam.married
+                individualFailed_list.append(wife)
+    return individualFailed_list
