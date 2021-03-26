04d7e77f2ad913e6bcc664090ce72055d16f2b25
Gireesh <gireeshsinghthakurathi@gmail.com>
2021-03-25 23:35:51 -0400

    Auto Test Sprint 2

--- autoTest_sprint2.py
+++ autoTest_sprint2.py
@@ -8,29 +8,29 @@ import datetime
 class UserStory5_AutoTest(unittest.TestCase):
 
     def test1(self):
-        individualErrors = sprint2.UserStory5()
+        individualFailed_list = sprint2.UserStory5()
 
-        for person in individualErrors:
+        for person in individualFailed_list:
             print("UserStory5: ERROR ENCOUNTERED: " + person.id + " Name " + person.name + " Death DAY "+ str(person.death) + " should not be before Marriage Date " + str(person.marriage))
 
-        self.assertTrue(len(individualErrors) == 0, "All marriages occur before deaths.")
+        self.assertTrue(len(individualFailed_list) == 0, "All marriages occur before deaths.")
 
 # Unit test for UserStory7
 # Author: Gireesh Singh Thakurathi
 class UserStory7_AutoTest(unittest.TestCase):
 
     def test1(self):
-        deathError, individualErrors = sprint2.UserStory7()
+        deathFailed_list, peopleFailed_list = sprint2.UserStory7()
         t = datetime.datetime.today()
         today = datetime.date(t.year, t.month, t.day)
 
-        for person in deathError:
+        for person in deathFailed_list:
             print("UserStory7: ERROR ENCOUNTERED: " + person.id + " Name " + person.name + " death "+ str(person.death) + " should not be greater than 150 years after birth: " + str(person.birthday))
 
-        for person in individualErrors:
+        for person in peopleFailed_list:
             print("UserStory7: ERROR ENCOUNTERED: " + person.id + " Name " + person.name + " birth day "+ str(person.birthday) + " should be less than 150 years from today: "+ today)
 
-        self.assertTrue(len(deathError) == 0 and len(individualErrors) == 0, "All deaths occur less than 150 years after birth and current date is also less than 150 years after all birth")
+        self.assertTrue(len(deathFailed_list) == 0 and len(peopleFailed_list) == 0, "All deaths occur less than 150 years after birth and current date is also less than 150 years after all birth")
 
 
 if __name__ == '__main__':
